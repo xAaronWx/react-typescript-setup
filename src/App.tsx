9e6ac170ada121cb3d368f00c06d682290c035a2
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-04-06 17:28:07 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Clock from "./components/Clock";
 
-function App() {
+let testProp: string = "Am I getting passed the Clock component?";
+let optionalProp: string = "You sure are!";
+
+const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="verticalCenter"></div>
+      <Clock testProp={testProp} optionalProp={optionalProp} />
     </div>
   );
-}
+};
 
 export default App;
